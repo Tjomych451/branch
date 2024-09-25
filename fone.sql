05224e3452cf2c7a378a34e5441ec7a6e7164d87
Tjomych451 <art115665@rambler.ru>
2024-09-25 22:54:46 +0300

    Add files via upload

---
+++ fone.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `py41`.`telefon_market` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `ProductName` VARCHAR(45) NOT NULL,
+  `Manefacturer` VARCHAR(45) NOT NULL,
+  `ProductCount` VARCHAR(45) NOT NULL,
+  `Price` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+NSERT INTO `telefon`.`telefon` (`ProductName`, `Manufacturer`, `ProductCount`, `Price`) VALUES ('iPhone X', 'Apple', '3', '76000');
+INSERT INTO `telefon`.`telefon` (`ProductName`, `Manufacturer`, `ProductCount`, `Price`) VALUES ('iPhone 8', 'Apple', '2', '51000');
+INSERT INTO `telefon`.`telefon` (`ProductName`, `Manufacturer`, `ProductCount`, `Price`) VALUES ('Galaxy S9', 'Samsung', '2', '56000');
+INSERT INTO `telefon`.`telefon` (`ProductName`, `Manufacturer`, `ProductCount`, `Price`) VALUES ('Galaxy S8', 'Samsung', '1', '41000');
+INSERT INTO `telefon`.`telefon` (`ProductName`, `Manufacturer`, `ProductCount`, `Price`) VALUES ('P 20 Pro', 'Huawei', '5', '36000');
